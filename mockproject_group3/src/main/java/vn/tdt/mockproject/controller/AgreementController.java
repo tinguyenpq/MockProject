29e0528cd0590d1d66dd674b49df82ffeece9ac6
tinguyenpq <tinguyenpq@gmail.com>
2015-08-08 09:37:59 +0700

    add agreement
    Signed-off-by: tinguyenpq <tinguyenpq@gmail.com>

---
+++ mockproject_group3/src/main/java/vn/tdt/mockproject/controller/AgreementController.java
@@ -0,0 +1,16 @@
+/**
+ * (c)Copyright 2015, ThanhTien. All rights reserved.
+ */
+package vn.tdt.mockproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * AggreementController.java
+ * @author ThanhTien
+ * @since 08-08-2015
+ */
+@Controller
+public class AgreementController {
+
+}
