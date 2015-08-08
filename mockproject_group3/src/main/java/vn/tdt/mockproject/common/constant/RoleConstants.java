30f9a4838684c5c21fbc717c8486a420ded0d1bd
tinguyenpq <tinguyenpq@gmail.com>
2015-08-08 08:46:34 +0700

    edit roles

--- mockproject_group3/src/main/java/vn/tdt/mockproject/common/constant/RoleConstants.java
+++ mockproject_group3/src/main/java/vn/tdt/mockproject/common/constant/RoleConstants.java
@@ -5,9 +5,47 @@ package vn.tdt.mockproject.common.constant;
 
 /**
  * RoleConstants.java
+ * 
  * @author ThanhTien
  * @since 07-08-2015
  */
 public class RoleConstants {
+	/**
+	 * Read Only User Roles
+	 */
+	public static final String ROLE_READ_ONLY_USER = "Read Only User";
+	/**
+	 * Dealer Roles
+	 */
+	public static final String ROLE_DEALER = "Dealer";
+	/**
+	 * Dealer Proposer Roles
+	 */
+	public static final String ROLE_DEALER_PROPOSER = "Dealer Proposer";
+	/**
+	 * CMS Roles
+	 */
+	public static final String ROLE_CSM = "CMS";
+	/**
+	 * Corporate Management Team Roles
+	 */
+	public static final String ROLE_CORPORATE_MANAGEMENT = "Corporate Management Team";
+	/**
+	 * Corporate Sales Administrator Roles
+	 */
+	public static final String ROLE_CORPORATE_SALES = "Corporate Sales Administrator";
+	/**
+	 * System Admin Roles
+	 */
+	public static final String ROLE_SYSTEM_ADMIN = "System Admin";
+	/**
+	 * LSM Roles
+	 */
+	public static final String ROLE_LSM = "LSM";
+	/**
+	 * anonymousUser Roles
+	 */
+	public static final String ROLE_ANONYMOUS_USER = "anonymousUser";
+	
 
 }
