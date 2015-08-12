4d8d4f7875ac9cd51716c14706f07a0497ed8660
tinguyenpq <tinguyenpq@gmail.com>
2015-08-12 21:15:50 +0700

    commit value of system config

--- mockproject_group3/src/main/java/vn/tdt/mockproject/common/constant/ValueConstants.java
+++ mockproject_group3/src/main/java/vn/tdt/mockproject/common/constant/ValueConstants.java
@@ -10,5 +10,75 @@ package vn.tdt.mockproject.common.constant;
  * @since 07-08-2015
  */
 public class ValueConstants {
+	/**
+	 * Value sytem config id of payment to
+	 */
+	public static final int SYSTEM_CONFIG_PAYMENT_TO = 1;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_DEALER_VISIBILITY = 2;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_DISCOUNT_UNIT = 3;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_COMBINABILITY = 4;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_TRIGGER_CREDIT = 5;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_CRM_AGREEMENT_FIL_LOCATION = 6;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_FUNDING_TYPE = 7;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_FUNDING_STATUS = 8;
+
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_ARCHIVE_YEARS = 9;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_SCHEDULED_DATES = 1;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_VOLUME_DISCOUNT_TYPE = 11;
+
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_BUSINESS_AREA = 12;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_NUMBER_LEVEL = 13;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_SIGNED_CONTRACT_DEFAULT = 14;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_ORDER_TYPE = 15;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_REG_TYPE = 16;
+	/**
+	 * Value sytem config id of
+	 */
+	public static final int SYSTEM_CONFIG_FUNDING_MATCH_BASED_ON = 17;
 
 }
